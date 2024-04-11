0bee8a4e3dd8a4b9229fd472f32f0c604711b1a7
supaicy <143978573+supaicy@users.noreply.github.com>
2024-04-11 17:53:32 +0900

    [Feat]: GatewayServiceImpl 구현

--- src/main/java/live/smoothing/ruleengine/gateway/service/impl/GatewayServiceImpl.java
+++ src/main/java/live/smoothing/ruleengine/gateway/service/impl/GatewayServiceImpl.java
@@ -2,26 +2,43 @@ package live.smoothing.ruleengine.gateway.service.impl;
 
 import live.smoothing.ruleengine.gateway.dto.GatewayGenerateRequest;
 import live.smoothing.ruleengine.gateway.entity.Gateway;
+import live.smoothing.ruleengine.gateway.repository.GatewayRepository;
 import live.smoothing.ruleengine.gateway.service.GatewayService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service("gatewayService")
+@AllArgsConstructor
 public class GatewayServiceImpl implements GatewayService {
+
+    private final GatewayRepository gatewayRepository;
 
     @Override
     public void addGateway(GatewayGenerateRequest request) {
 
+        String ip = request.getGatewayIp();
+        String name = request.getGatewayName();
+
+        if(gatewayRepository.existsByGatewayIpOrGatewayNameExists(ip, name)) {
+
+            Gateway gateway = new Gateway(null, request.getGatewayIp(), request.getGatewayPort(), request.getGatewayName(), request.getGatewayType());
+            gatewayRepository.save(gateway);
+        }
     }
 
     @Override
     public String getGatewayName(Integer gatewayId) {
-        return "";
+
+        return gatewayRepository.findById(gatewayId)
+                .get()
+                .getGatewayName();
     }
 
     @Override
     public List<Gateway> getGateways() {
-        return List.of();
+
+        return gatewayRepository.findAll();
     }
 }
