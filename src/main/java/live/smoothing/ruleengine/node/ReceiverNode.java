385254527dfe5bec520ec3dfd3db062d0bd19c60
doramonz <whs3867@icloud.com>
2024-04-17 15:11:03 +0900

    [Feature] : Receiver Node

--- src/main/java/live/smoothing/ruleengine/node/ReceiverNode.java
+++ src/main/java/live/smoothing/ruleengine/node/ReceiverNode.java
@@ -1,23 +1,48 @@
 package live.smoothing.ruleengine.node;
 
+import live.smoothing.ruleengine.sensor.dto.SensorMessage;
+
+/**
+ * Receiver Node
+ * 하는 거 없음
+ *
+ * @author 우혜승
+ */
 public class ReceiverNode extends Node {
-    private int outputPortCount;
 
     protected ReceiverNode(String nodeId, int outputPortCount) {
         super(nodeId, outputPortCount);
-        this.outputPortCount = outputPortCount;
 
     }
 
+    /**
+     * 메시지 수신
+     */
     @Override
     public void run() {
-        for(int i = 0; i < outputPortCount; i++) {
+        while(!Thread.currentThread().isInterrupted()) {
             try {
-                SensorMessage message = getInputWire().get();
-                outputPorts[i].getWire(0).put(message);
+                SensorMessage message = tryGetMessage();
+                process(message);
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+        }
+
+    }
+
+    /**
+     * 수신 후 메시지 다음 노드로 전달
+     * @param message 수신한 메시지
+     */
+    private void process(SensorMessage message) {
+        for(int i = 0; i < getOutputPortCount(); i++) {
+            try {
+                output(i, message);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
             }
         }
     }
+
 }
