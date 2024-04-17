220a68a61197ecacf9172d5670841e8c6b33cabd
supaicy <143978573+supaicy@users.noreply.github.com>
2024-04-17 16:43:42 +0900

    [Fix]: Topic(String)을 파싱하는 부분 수정

--- src/main/java/live/smoothing/ruleengine/node/TopicParsingNode.java
+++ src/main/java/live/smoothing/ruleengine/node/TopicParsingNode.java
@@ -2,19 +2,16 @@ package live.smoothing.ruleengine.node;
 
 import live.smoothing.ruleengine.sensor.dto.SensorMessage;
 import lombok.SneakyThrows;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.json.JsonParser;
 import org.springframework.boot.json.JsonParserFactory;
 
 import java.util.Map;
 
-@Slf4j
 public class TopicParsingNode extends Node {
-    private static final String ATTRIBUTE_TIME = "time";
-    private static final String ATTRIBUTE_VALUE = "value";
-    private static final String ATTRIBUTE_TOPIC = "topic";
-    private static final String ATTRIBUTE_PAYLOAD = "payload";
 
+    private static final Logger log = LoggerFactory.getLogger(TopicParsingNode.class);
     private final Map<String, String> map;
 
     protected TopicParsingNode(String nodeId, int outputPortCount, Map<String, String> map) {
@@ -23,51 +20,55 @@ public class TopicParsingNode extends Node {
         this.map = map;
     }
 
+    /**
+     * Runs this operation.
+     */
     @Override
     @SneakyThrows
     public void run() {
+        while(!Thread.currentThread().isInterrupted()) {
 
-        while (!Thread.currentThread().isInterrupted()) {
             try {
-                processMessage();
-            } catch (InterruptedException e) {
-                log.info("[토픽파싱노드]: Thread interrupted");
+
+                SensorMessage goal = tryGetMessage();
+
+                String topic = goal.getAttribute("topic");
+                String payload = goal.getAttribute("payload");
+
+                for(String key : map.keySet()) {
+
+                    String finalKey = map.get(key);
+                    String topicValue = letParseString(key, topic);
+                    goal.addAttribute(topicValue, finalKey);
+                }
+
+
+                goal.addAttribute("time", letParse("time", payload));
+                goal.addAttribute("value", letParse("value", payload));
+
+                for(int i = 0; i < getOutputPortCount(); i++) {
+
+                    output(i, goal);
+                }
+
+            } catch(InterruptedException e) {
+                log.info("토픽파싱노드 Interrupted");
             }
         }
     }
 
-    private void processMessage() throws InterruptedException {
-        SensorMessage message = tryGetMessage();
-        parseTopicAndPayload(message);
-        outputMessageToManyNodes(message);
-    }
+    private static String letParse(String key, String target) {
 
-    private void parseTopicAndPayload(SensorMessage message) {
-        String topic = message.getAttribute(ATTRIBUTE_TOPIC);
-        String payload = message.getAttribute(ATTRIBUTE_PAYLOAD);
-
-        for (String key : map.keySet()) {
-            String finalKey = map.get(key);
-            String topicValue = parseJson(key, topic);
-
-            message.addAttribute(finalKey, topicValue);
-        }
-
-        String time = parseJson(ATTRIBUTE_TIME, payload);
-        String value = parseJson(ATTRIBUTE_VALUE, payload);
-
-        message.addAttribute(ATTRIBUTE_TIME, time);
-        message.addAttribute(ATTRIBUTE_VALUE, value);
-    }
-
-    private void outputMessageToManyNodes(SensorMessage message) throws InterruptedException {
-        for (int i = 0; i < getOutputPortCount(); i++) {
-            output(i, message);
-        }
-    }
-
-    private static String parseJson(String key, String target) {
         JsonParser jsonParser = JsonParserFactory.getJsonParser();
         return jsonParser.parseMap(target).get(key).toString();
+    }
+
+    private static String letParseString(String key, String target) {
+
+        String step = target.split(key+"/")[1];
+        if(step.contains("/")) {
+            return step.split("/")[0];
+        }
+        return step;
     }
 }
