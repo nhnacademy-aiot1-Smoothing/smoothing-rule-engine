615ed12450e3d50be81851857130b35c4a1d6003
supermicrosoft <30441597+supermicrosoft@users.noreply.github.com>
2024-04-18 19:32:59 +0900

    [Feat]: TopicResponseDto 구현

---
+++ src/main/java/live/smoothing/ruleengine/response/TopicResponseDto.java
@@ -0,0 +1,14 @@
+package live.smoothing.ruleengine.response;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TopicResponseDto {
+
+    @NonNull
+    private String topic;
+
+}
