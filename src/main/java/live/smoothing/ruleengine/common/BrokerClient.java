6f7151ca5e9620ef69cd1a17133399883aec9aa8
supaicy <143978573+supaicy@users.noreply.github.com>
2024-04-19 13:09:58 +0900

    [Remove]: Openfeign 인터페이스 parameter 제거

--- src/main/java/live/smoothing/ruleengine/common/BrokerClient.java
+++ src/main/java/live/smoothing/ruleengine/common/BrokerClient.java
@@ -3,14 +3,13 @@ package live.smoothing.ruleengine.common;
 import live.smoothing.ruleengine.response.BrokerResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Optional;
+import java.util.List;
 
 @FeignClient(name = "broker-detail-service", path = "/api/broker")
 public interface BrokerClient {
 
     @GetMapping("/search/loadBrokerByBrokerId")
-    public Optional<BrokerResponseDto> loadBrokerByBrokerId(@RequestParam Integer brokerId);
+    public List<BrokerResponseDto> loadBrokerByBrokerId();
 
 }
