ccf1d7c94f0bb4e3536b2ae610d6147d45b44a40
doramonz <whs3867@gmail.com>
2024-05-22 16:51:43 +0900

    [Fix] : LocalDateTime to String

--- src/main/java/live/smoothing/ruleengine/sensor/dto/SensorErrorRequest.java
+++ src/main/java/live/smoothing/ruleengine/sensor/dto/SensorErrorRequest.java
@@ -1,19 +1,25 @@
 package live.smoothing.ruleengine.sensor.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Getter
-@Builder
-@AllArgsConstructor
 public class SensorErrorRequest {
 
     private String sensorErrorType;
-    private LocalDateTime createdAt;
+    private String createdAt;
     private Double sensorValue;
     private Integer sensorId;
     private String topic;
+
+    @Builder
+    public SensorErrorRequest(String sensorErrorType, LocalDateTime createdAt, Double sensorValue, Integer sensorId, String topic) {
+        this.sensorErrorType = sensorErrorType;
+        this.createdAt = createdAt.toString();
+        this.sensorValue = sensorValue;
+        this.sensorId = sensorId;
+        this.topic = topic;
+    }
 }
