7311d468057efaea5904bd595aeb6f8684bef5a3
doramonz <whs3867@gmail.com>
2024-05-28 09:08:19 +0900

    [Feature] : Add Checker

---
+++ src/main/java/live/smoothing/ruleengine/node/checker/LowerChecker.java
@@ -0,0 +1,26 @@
+package live.smoothing.ruleengine.node.checker;
+
+import live.smoothing.ruleengine.common.Parameters;
+import live.smoothing.ruleengine.sensor.dto.SensorMessage;
+
+import java.util.Objects;
+
+public class LowerChecker implements Checker {
+    private final String key;
+    private final Long value;
+
+    public LowerChecker(Parameters parameters) {
+        this.key = parameters.getParam("key");
+        this.value= Long.valueOf(parameters.getParam("value"));
+    }
+
+    @Override
+    public boolean check(SensorMessage message) {
+        Object targetValue = message.getAttribute(key);
+        if(Objects.isNull(targetValue) || !(targetValue instanceof Long || targetValue instanceof Integer || targetValue instanceof Short || targetValue instanceof Byte || targetValue instanceof Double || targetValue instanceof Float)) {
+            return false;
+        }
+
+        return (Long) targetValue < value;
+    }
+}
