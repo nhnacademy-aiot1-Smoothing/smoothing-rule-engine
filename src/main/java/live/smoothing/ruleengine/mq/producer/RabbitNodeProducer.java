cbfad388720c308c05bac7d953af6d04ed8816a6
doramonz <whs3867@gmail.com>
2024-06-17 22:17:00 +0900

    [Fix] : 에러처리

--- src/main/java/live/smoothing/ruleengine/mq/producer/RabbitNodeProducer.java
+++ src/main/java/live/smoothing/ruleengine/mq/producer/RabbitNodeProducer.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.annotation.Profile;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -25,9 +26,13 @@ public class RabbitNodeProducer implements NodeProducer{
 
     @Override
     public void generateQueue(String key) {
+        try{
         rabbitTemplate.execute(channel -> {
             channel.queueDeclare(key, true, false, false, null);
             return null;
         });
+        }catch (Exception e){
+            log.error("generate queue error : {}", e.getMessage());
+        }
     }
 }
